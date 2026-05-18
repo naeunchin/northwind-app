3a2042e67ce7ff91e9081570d1f3c8aef7425035
naeunchin <naeunchin@gmail.com>
2026-05-17 21:52:24 -0600

    Added custom feedback and error messages display page

---
+++ BlazorWebApp/Components/Custom/FeedbackAndErrorDisplay.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebApp.Components.Custom
+{
+    public partial class FeedbackAndErrorDisplay
+    {
+        //We can have parameters for custom components, as pages do. Parameters can be used to pass data from the parent (page) to the component. 
+        #region Parameters
+        [Parameter]
+        public List<string> ErrorDetails { get; set; } = [];
+
+        [Parameter]
+        public string ErrorMessage { get; set; } = string.Empty;
+
+        [Parameter]
+        public string FeedbackMessage { get; set; } = string.Empty;
+
+        private bool hasError => !string.IsNullOrEmpty(ErrorMessage) || ErrorDetails.Any();
+        #endregion
+    }
+}
