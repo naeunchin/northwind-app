4dc1fe315152f688d70bb898fa15fc43e7aedb87
naeunchin <naeunchin@gmail.com>
2026-05-17 21:27:44 -0600

    Added product and order pages

---
+++ BlazorWebApp/Components/Pages/AppPages/Inventory.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using OLTPSystem.BLL;
+using OLTPSystem.ViewModels;
+
+namespace BlazorWebApp.Components.Pages.AppPages
+{
+    public partial class Inventory
+    {
+        #region Fields
+        private string productName = string.Empty;
+        private int categoryID = 0;
+        private string feedbackMessage = string.Empty;
+        private List<string> errorDetails = new();
+        private string errorMessage = string.Empty;
+        private List<ProductView> ProductList = [];
+        #endregion
+
+        #region Injection & Properties
+        [Inject]
+        protected ProductService ProductService { get; set; } = default!;
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; } = default!;
+
+        private bool hasError => !string.IsNullOrEmpty(errorMessage) || errorDetails.Any();
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Performs the asynchronous lookup of product inventory data based on the user's current selection filter input.
+        /// </summary>
+        public void GetProducts()
+        {
+            errorDetails.Clear();
+            errorMessage = string.Empty;
+            feedbackMessage = string.Empty;
+            ProductList = [];
+
+            try
+            {
+                BYSResults.Result<List<ProductView>> result;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+        public void EditProduct(int productID)
+        {
+            NavigationManager.NavigateTo($"/ProductEdit/{productID}");
+        }
+        public void NewProduct()
+        {
+            NavigationManager.NavigateTo("/ProductEdit/0");
+        }
+        #endregion
+    }
+}
---
+++ BlazorWebApp/Components/Pages/AppPages/Orders.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using OLTPSystem.BLL;
+using OLTPSystem.ViewModels;
+
+namespace BlazorWebApp.Components.Pages.AppPages
+{
+    public partial class Orders
+    {
+        #region Fields
+        #endregion
+
+        #region Injection & Properties
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
